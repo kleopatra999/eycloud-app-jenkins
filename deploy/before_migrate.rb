ffe5d10d4095386dceb3d86e8d0aedc2975170ee
Luis Lavena <luislavena@gmail.com>
2013-12-23 20:24:22 -0300

    Update syntax used by EY gem 2.3

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,13 +1,14 @@
 # Set up a chef 0.10 dna.json file (for stack-v1 + stack-v2)
 # TODO does this run on non-app-master/solo?
-custom_json = node.dup
+custom_json = config.node.dup
 custom_json['run_list'] = 'recipe[main]'
-File.open("/etc/chef-custom/dna.json", 'w') do |f|
+
+dna_path = "/etc/chef-custom/dna.json"
+File.open(dna_path, 'w') do |f|
   f.puts JSON.pretty_generate(custom_json)
   f.chmod(0600)
 end
 
 # Runs application cookbooks
-run "cd #{latest_release}; sudo bundle exec chef-solo -c #{latest_release}/deploy/solo.rb -j /etc/chef-custom/dna.json"
-
-run "cat #{latest_release}/deploy/chef-stacktrace.out 2> /dev/null || echo 'Chef run successful'"
+run "cd #{config.release_path} && sudo bundle exec chef-solo -c #{config.release_path}/deploy/solo.rb -j #{dna_path}"
+run "cat #{config.release_path}/deploy/chef-stacktrace.out 2> /dev/null || echo 'Chef run successful'"
