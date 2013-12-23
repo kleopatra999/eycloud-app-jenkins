1c4a50eab2a302cbbe7a9354180bf7736c116f94
Luis Lavena <luislavena@gmail.com>
2013-12-23 20:29:36 -0300

    Attempt to generate DNA file locally and then use sudo to copy it

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,12 +3,15 @@
 custom_json = config.node.dup
 custom_json['run_list'] = 'recipe[main]'
 
-dna_path = "/etc/chef-custom/dna.json"
+dna_path = File.join(config.release_path, "deploy", "dna.json")
+dna_target = "/etc/chef-custom/dna.json"
+
 File.open(dna_path, 'w') do |f|
   f.puts JSON.pretty_generate(custom_json)
   f.chmod(0600)
 end
 
 # Runs application cookbooks
-run "cd #{config.release_path} && sudo bundle exec chef-solo -c #{config.release_path}/deploy/solo.rb -j #{dna_path}"
+sudo "cp -f #{dna_path} #{dna_target}"
+run "cd #{config.release_path} && sudo bundle exec chef-solo -c #{config.release_path}/deploy/solo.rb -j #{dna_target}"
 run "cat #{config.release_path}/deploy/chef-stacktrace.out 2> /dev/null || echo 'Chef run successful'"
